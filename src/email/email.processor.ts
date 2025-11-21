058f1ce2257d334f782b3c5b4eca4e0a4715c723
Grzesiek <g.maciejczak95@gmail.com>
2025-11-21 15:56:43 +0100

    added emails working

--- src/email/email.processor.ts
+++ src/email/email.processor.ts
@@ -17,18 +17,35 @@ export class EmailProcessor extends WorkerHost {
   async process(job: Job<RepairsFormDto>) {
     console.log(`Dane: ${JSON.stringify(job.data)}`);
     const data = job.data;
-    await this.mailerService.sendMail({
-      replyTo: data.email,
-      to: 'serwis@repear.pl',
-      subject: `Nowe zgłoszenie naprawy: ${data.serialNumber}`,
-      template: 'repair-form',
-      context: {
-        email: data.email,
-        description: data.description,
-        serialNumber: data.serialNumber,
-        phone: data.phone,
-      },
-    });
-    console.log(`Email wysłany do ${data.email}`);
+
+    try {
+      await this.mailerService.sendMail({
+        replyTo: data.email,
+        to: `Repear.pl - Serwis urządzeń Apple <${this.configService.get<string>('MAILER_USER')}>`,
+        subject: `Nowe zgłoszenie naprawy: ${data.serialNumber}`,
+        template: 'repair-form',
+        context: {
+          email: data.email,
+          description: data.description,
+          serialNumber: data.serialNumber,
+          phone: data.phone,
+        },
+      });
+      console.log(`Email wysłany do ${data.email}`);
+
+      await this.mailerService.sendMail({
+        to: data.email,
+        subject: `Potwierdzenie zgłoszenia naprawy: ${data.serialNumber}`,
+        template: 'thank-you-repair-form',
+        context: {
+          email: data.email,
+          serialNumber: data.serialNumber,
+          phone: data.phone,
+          description: data.description,
+        },
+      });
+    } catch (error) {
+      console.error(`Błąd podczas wysyłania emaili: ${error.message}`);
+    }
   }
 }
